ac8488fe6ba8a3551fc81985df45caa5cc8951c0
Lilianne <marixko@gmail.com>
2023-11-07 00:02:56 +0000

    excluded unused paths

--- settings/paths.py
+++ settings/paths.py
@@ -1,6 +1,6 @@
 import os
 
-release="CHANCES_XPSP_GAIA_BR"
+# release="CHANCES_XPSP_GAIA_BR"
 
 splus_path = os.path.join(os.path.sep,"storage","splus")
 code_path = os.path.join(os.path.sep, "storage", "splus", "scripts", "sqg_idr5")
@@ -15,7 +15,7 @@ sqg_path = os.path.join(splus_path, "Catalogues", "VACs", "sqg")
 
 # wise_path = os.path.join(sqg_path, release, "wise_match")
 # gaia_wise_path = os.path.join(sqg_path, release, "gaia_wise_match")
-original_path = os.path.join(sqg_path, release, "original")
-output_folder = os.path.join(sqg_path, release)
+# original_path = os.path.join(sqg_path, release, "original")
+# output_folder = os.path.join(sqg_path, release)
 # output_path = os.path.join(sqg_path, release, "final_output")
 
